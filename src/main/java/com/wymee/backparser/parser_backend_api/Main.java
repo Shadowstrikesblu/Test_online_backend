d994272337b90bb360671c11e205732837355f1b
Shadowstrikesblu <hmoulk8@gmail.com>
2023-02-22 14:56:36 +0100

    14

--- src/main/java/com/wymee/backparser/parser_backend_api/Main.java
+++ src/main/java/com/wymee/backparser/parser_backend_api/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World 2 ");
+        System.out.println("Hello World ");
         SpringApplication.run(Main.class, args);
     }
 
