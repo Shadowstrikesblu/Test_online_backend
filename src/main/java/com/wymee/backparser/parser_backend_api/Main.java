7fee82ec999b79d60fbc64ac4b758d5e67a36d6b
Shadowstrikesblu <hmoulk8@gmail.com>
2023-02-24 16:13:49 +0100

    14

--- src/main/java/com/wymee/backparser/parser_backend_api/Main.java
+++ src/main/java/com/wymee/backparser/parser_backend_api/Main.java
@@ -3,16 +3,19 @@ package com.wymee.backparser.parser_backend_api;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
 public class Main {
-
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World 3 ");
         SpringApplication.run(Main.class, args);
+
     }
 
 }
